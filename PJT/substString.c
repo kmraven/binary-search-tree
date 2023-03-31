9554797de84975086aaa3be118b63fcc0a6d75f1
Ryota Kimura <rkimura@fun.bio.keio.ac.jp>
2023-03-31 19:06:30 +0900

    substの処理もっと簡潔にした

--- PJT/substString.c
+++ PJT/substString.c
@@ -10,14 +10,14 @@ int substString(BinSTreeNode* rootNode, const char *sstr, const char *rstr, int 
         // 間順走査
         replaced = substString(rootNode->left, sstr, rstr, replaced); // 左ノードへ移動
 
-        char *before, *after;
-        before = (char*)malloc(sizeof(char) * (mystrlen(rootNode->word) + 1));
-        mystrcpy(before, rootNode->word);
-        after = mystrsubst(rootNode->word, sstr, rstr); // substの返り値がmallocされてるので変数に格納しといてfreeする
-        free(rootNode->word);
-        rootNode->word = after;
-        if(mystrcmp(before, rootNode->word) != 0) {replaced += 1;} // 変わってたらreplacedに1を加算
-        free(before);
+        char *after = mystrsubst(rootNode->word, sstr, rstr); // substの返り値がmallocされてるので変数に格納しといてfreeする
+        if(mystrcmp(rootNode->word, after) != 0) {
+            free(rootNode->word);
+            rootNode->word = after;
+            replaced += 1; // 変わってたらreplacedに1を加算
+        } else {
+            free(after);
+        }
 
         replaced = substString(rootNode->right, sstr, rstr, replaced); // 右ノードへ移動
     }
